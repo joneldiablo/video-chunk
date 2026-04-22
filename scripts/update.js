338bbdfdbaacb7cae2d8ac5b91581f1dbc17fad5
jonathan diego rdz <jon_eldiablo@hotmail.com>
2026-04-21 19:57:03 -0600

    path

--- scripts/update.js
+++ scripts/update.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
-const p = require('./package.json');
+const p = require('../package.json');
 
 const parts = p.version.split('.');
 parts[parts.length - 1] = 1 + (Number(parts[parts.length - 1]));
 p.version = parts.join('.');
 
-fs.writeFileSync('./package.json', JSON.stringify(p, null, 2));
+fs.writeFileSync('../package.json', JSON.stringify(p, null, 2));
